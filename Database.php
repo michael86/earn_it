45f3cd20b78fffa9b75af4ebdc5449502ad9da21
michael86 <michael8t6@gmail.com>
2025-12-23 01:22:16 +0000

    pass options to PDO connection

--- Database.php
+++ Database.php
@@ -12,11 +12,12 @@ class Database {
         $dsn = "mysql:host={$cnf['DB_HOST']};port={$cnf['DB_PORT']};dbname={$cnf['DB_NAME']}";
 
         $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
  
         try {
-            $this->conn = new PDO($dsn, $cnf['DB_USER'], $cnf['DB_PASS']);
+            $this->conn = new PDO($dsn, $cnf['DB_USER'], $cnf['DB_PASS'], $options);
         } catch (PDOException $e) {
             throw new Exception("Databse connection failed: {$e->getMessage()}");
         }
